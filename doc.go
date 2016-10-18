aa17d874a8e99554df890007367f12d23f4f5727
Qin Xiao <xiaoq08@163.com>
2016-10-18 17:25:57 +0800

    fix README

--- doc.go
+++ doc.go
@@ -1,10 +1,6 @@
 /*
 包 qiniupkg.com/kirk 是七牛容器云产品 KIRK 相关的 Golang 开源代码库，包含对接产品 API 接口的 kirksdk 等。
 
-例如：
-
-	import "qiniupkg.com/kirk/kirsdk"
-
 和七牛云其他产品相同，管理容器云资源需要密钥 AccessKey/SecretKey，请注意保证账号密钥的安全。
 */
 package kirk
