5b04f2dd234d082365b4ac0085a8fd5e5189e2e2
longwenlu@gmail.com <nowen@192.168.0.105>
2016-10-31 01:31:39 +0800

    QCOS-2933 Update readme.md

--- README.md
+++ README.md
@@ -1,11 +1,100 @@
 [![Qiniu Logo](http://open.qiniudn.com/logo.png)](http://qiniu.com/)
 
-QINIU KIRK
-===============
+# QINIU KIRK
 [![GoDoc](https://godoc.org/qiniupkg.com/kirk?status.svg)](https://godoc.org/qiniupkg.com/kirk)
 
-文档
-===============
+# 简介
+本 SDK 基于 golang 语言， 用于与 QINIU KIRK 通用计算平台 REST API 的编程交互，提供了在开发者业务服务器（服务端或客户端）管理七牛容器云资源的能力。
+
+# 安装
+> 本 SDK 需要 [go 1.6](https://golang.org/dl/) 以上版本
+
+## 使用 go get 安装
+```
+$ go get -u qiniupkg.com/kirk/kirksdk
+```
+
+## 使用 [glide](https://glide.sh) 安装
+- 安装 [glide](https://glide.sh) 包管理工具
+- 在项目有中添加一个 import “qiniupkg.com/kirk/kirksdk” 的 .go 源文件，并执行如下命令。glide会自动扫描代码并下载需要的包
+```
+$ cd your_project_dir
+$ glide init
+$ glide install
+```
+# 示例
+## 创建 App
+```golang
+import "qiniupkg.com/kirk/kirksdk"
+
+...
+
+cfg := kirksdk.AccountConfig{
+	AccessKey: ACCESS_KEY,
+	SecretKey: SECRET_KEY,
+	Host:      kirksdk.DefaultAccountHost,
+}
+
+accountClient := kirksdk.NewAccountClient(cfg)
+
+createdApp, err := accountClient.CreateApp(context.TODO(), "myapp", kirksdk.CreateAppArgs{
+	Title:  "title",
+	Region: "nq",
+})
+
+if err != nil {
+// 错误处理
+}
+
+fmt.Println(createdApp.URI)
+```
+
+## 在 App 下创建 Service
+```golang
+import "qiniupkg.com/kirk/kirksdk"
+
+...
+
+qcosClient, err := accountClient.GetQcosClient(context.TODO(), createdApp.URI)
+if err != nil {
+// 错误处理
+}
+
+err = qcosClient.CreateService(context.TODO(), "mystack", kirksdk.CreateServiceArgs{
+	Name: "myservice",
+})
+if err != nil {
+// 错误处理
+}
+```
+
+## 列出账号下所有镜像仓库
+```golang
+import "qiniupkg.com/kirk/kirksdk"
+
+...
+
+accountInfo, err := accountClient.GetAccountInfo(context.TODO())
+if err != nil {
+// 错误处理
+}
+
+indexClient, err := accountClient.GetIndexClient(context.TODO())
+if err != nil {
+// 错误处理
+}
+
+repos, err := indexClient.ListRepo(context.TODO(), accountInfo.Name)
+if err != nil {
+// 错误处理
+}
+
+for _, repo := range repos {
+	fmt.Println(repo.Name)
+}
+```
+
+# 相关文档
 - [qiniupkg.com/kirk](https://godoc.org/qiniupkg.com/kirk)
 - [开放API和SDK文档](http://kirk-docs.qiniu.com/apidocs/?go)
 - [产品文档](http://kirk-docs.qiniu.com/)
--- kirksdk/account_api.go
+++ kirksdk/account_api.go
@@ -9,13 +9,19 @@ import (
 )
 
 const (
-	KeyStateEnabled  = "enabled"
+	// KeyStateEnabled 表示key的启用状态
+	KeyStateEnabled = "enabled"
+
+	// KeyStateDisabled 表示key的禁用状态
 	KeyStateDisabled = "disabled"
 )
 
 const (
+	// DefaultAccountHost 表示默认的账号 REST API 端点
 	DefaultAccountHost = "https://app-api.qiniu.com"
-	DefaultIndexHost   = "https://index.qiniu.com"
+
+	// DefaultIndexHost 表示默认的镜像 REST API 端点
+	DefaultIndexHost = "https://index.qiniu.com"
 )
 
 const (
@@ -24,29 +30,59 @@ const (
 	ProductQcosGates    = "qcos_gates"
 )
 
+// AccountClient 包含针对账号 REST API 的各项操作
 type AccountClient interface {
+
+	// GetAccountInfo 用于得到 Account 的相关信息
 	GetAccountInfo(ctx context.Context) (ret AccountInfo, err error)
 
+	// CreateApp 用于在 Account 下创建 App
 	CreateApp(ctx context.Context, appName string, args CreateAppArgs) (ret AppInfo, err error)
+
+	// DeleteApp 用于删除 Account 下的 App
 	DeleteApp(ctx context.Context, appURI string) (err error)
+
+	// GetApp 用于得到 App 的相关信息
 	GetApp(ctx context.Context, appName string) (ret AppInfo, err error)
+
+	// GetAppKeys 用于得到 App 的 Key
 	GetAppKeys(ctx context.Context, appURI string) (ret []KeyPair, err error)
+
+	// ListApps 用于列出 Account 下的所有 App
 	ListApps(ctx context.Context) (ret []AppInfo, err error)
+
+	// ListManagedApps 用于列出 Account 下的所有 VendorManaged App
 	ListManagedApps(ctx context.Context) (ret []AppInfo, err error)
 
+	// GetRegion 用于得到 Region 相关的信息
 	GetRegion(ctx context.Context, regionName string) (ret RegionInfo, err error)
+
+	// ListRegions 用于列出所有可用的 Region
 	ListRegions(ctx context.Context) (ret []RegionInfo, err error)
 
+	// CreateAlertMethod 用于创建告警联系人
 	CreateAlertMethod(ctx context.Context, appURI string, args CreateAlertMethodArgs) (ret AlertMethodInfo, err error)
+
+	// DeleteAlertMethod 用于删除告警联系人
 	DeleteAlertMethod(ctx context.Context, appURI string, id string) (err error)
+
+	// GetAlertMethod 用于得到告警联系人相关信息
 	GetAlertMethod(ctx context.Context, appURI string, id string) (ret AlertMethodInfo, err error)
+
+	// ListAlertMethod 用于列出所有告警联系人
 	ListAlertMethod(ctx context.Context, appURI string) (ret []AlertMethodInfo, err error)
+
+	// UpdateAlertMethod 用于更新告警联系人
 	UpdateAlertMethod(ctx context.Context, appURI string, id string, args UpdateAlertMethodArgs) (ret AlertMethodInfo, err error)
 
+	// GetIndexClient 用于得到与镜像 REST API 交互的 IndexClient
 	GetIndexClient(ctx context.Context) (client IndexClient, err error)
+
+	// GetQcosClient 用于得到与某个 App 交互的 QcosClient
 	GetQcosClient(ctx context.Context, appURI string) (client QcosClient, err error)
 }
 
+// AccountConfig 包含创建 AccountClient 所需的信息
 type AccountConfig struct {
 	AccessKey string `json:"ak"`
 	SecretKey string `json:"sk"`
@@ -56,6 +92,7 @@ type AccountConfig struct {
 	Transport http.RoundTripper
 }
 
+// CreateAppArgs 包含创建一个 App 所需的信息
 type CreateAppArgs struct {
 	Title   string `json:"title"`
 	Region  string `json:"region"`
@@ -63,6 +100,7 @@ type CreateAppArgs struct {
 	SpecVer uint32 `json:"specVer"`
 }
 
+// AccountInfo 包含 Account 的相关信息
 type AccountInfo struct {
 	ID               uint32    `json:"id"`
 	Name             string    `json:"uri"`
@@ -71,6 +109,7 @@ type AccountInfo struct {
 	ModificationTime time.Time `json:"mtime"`
 }
 
+// AppInfo 包含 App 的相关信息
 type AppInfo struct {
 	ID               uint32    `json:"id"`
 	URI              string    `json:"uri"`
@@ -84,6 +123,7 @@ type AppInfo struct {
 	AppExtendedInfo
 }
 
+// AppExtendedInfo 包含 App 的相关扩展信息
 type AppExtendedInfo struct {
 	VendorURI string `json:"vendorUri,omitempty"`
 	SpecURI   string `json:"specUri,omitempty"`
@@ -91,25 +131,26 @@ type AppExtendedInfo struct {
 	SetupMode string `json:"setupMode,omitempty"`
 }
 
-/*
-	获得应用自身 access keys (access key id / secret key)
-*/
+// KeyPair 代表一对 Access Key， Secret Key 以及它们的启用状态
 type KeyPair struct {
 	AccessKey string `json:"ak"`
 	SecretKey string `json:"sk"`
 	State     string `json:"state"`
 }
 
+// RegionInfo 包含 Region 的相关信息
 type RegionInfo struct {
 	Name     string            `json:"name"`
 	Desc     string            `json:"desc"`
 	Products map[string]string `json:"products"`
 }
 
+// AlertMethods 代表若干个告警联系人
 type AlertMethods struct {
 	Methods []AlertMethodInfo `json:"methods"`
 }
 
+// AlertMethodInfo 包含告警联系人的相关信息
 type AlertMethodInfo struct {
 	ID          uint64 `json:"id"`
 	Owner       uint32 `json:"owner"`
@@ -120,6 +161,7 @@ type AlertMethodInfo struct {
 	Code        string `json:"code"`
 }
 
+// CreateAlertMethodArgs 包含创建告警联系人所需的信息
 type CreateAlertMethodArgs struct {
 	Name        string `json:"name"`
 	Email       string `json:"email"`
@@ -128,6 +170,7 @@ type CreateAlertMethodArgs struct {
 	Code        string `json:"code"`
 }
 
+// UpdateAlertMethodArgs 包含更新告警联系人所需的信息
 type UpdateAlertMethodArgs struct {
 	Name        string `json:"name"`
 	Email       string `json:"email"`
