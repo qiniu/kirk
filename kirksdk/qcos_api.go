4010da4337eecfb8ae15932b6d08299998c7e555
Qin Xiao <xiaoq08@163.com>
2016-12-02 20:16:24 +0800

    Revert "QCOS-0 fix host missing"

--- kirksdk/qcos_api.go
+++ kirksdk/qcos_api.go
@@ -311,9 +311,6 @@ type QcosClient interface {
 
 	// GET /v3/configservices/<namespace>
 	GetConfigServiceSpec(ctx context.Context, namespace string) (ret ConfigServiceSpecInfo, err error)
-
-	// POST /v3/configservices/<namespace>
-	UpdateConfigServiceSpec(ctx context.Context, namespace string, args UpdateConfigServiceSpecArgs) (err error)
 }
 
 const (
@@ -889,8 +886,3 @@ type CreateConfigServiceSpecArgs struct {
 }
 
 type ConfigServiceSpecInfo CreateConfigServiceSpecArgs
-
-type UpdateConfigServiceSpecArgs struct {
-	Vars     map[string]interface{}   `json:"vars"`
-	Listvars []map[string]interface{} `json:"listvars"`
-}
